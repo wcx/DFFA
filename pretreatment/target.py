8a072c283ac0f4e685fa95671697a387cca097f5
wcx <369808898@qq.com>
2016-04-25 15:35:19 +0800

    增加target的getter方法

--- pretreatment/target.py
+++ pretreatment/target.py
@@ -4,14 +4,43 @@
 
 class TestTarget(object):
     def __init__(self, package, activity, mime_type, file_name, app_name, version_code, version_name, seed):
-        self.package = package
-        self.activity = activity
-        self.file_name = file_name
-        self.mime_type = mime_type
-        self.app_name = app_name
-        self.version_code = version_code
-        self.version_name = version_name
-        self.seed = seed
+        self._package = package
+        self._activity = activity
+        self._file_name = file_name
+        self._mime_type = mime_type
+        self._app_name = app_name
+        self._version_code = version_code
+        self._version_name = version_name
+        self._seed = seed
 
+    @property
+    def package(self):
+        return self._package
 
+    @property
+    def activity(self):
+        return self._activity
 
+    @property
+    def file_name(self):
+        return self.file_name
+
+    @property
+    def mime_type(self):
+        return self._mime_type
+
+    @property
+    def app_name(self):
+        return self._app_name
+
+    @property
+    def version_code(self):
+        return self._version_code
+
+    @property
+    def version_name(self):
+        return self._version_name
+
+    @property
+    def seed(self):
+        return self._seed
