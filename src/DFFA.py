7245be802bd969884964a93d8b9087092db1c860
wcx <369808898@qq.com>
2016-06-23 17:45:21 +0800

    将run_job里的代码抽象成各个方法

--- src/DFFA.py
+++ src/DFFA.py
@@ -20,8 +20,7 @@ def get_uni_crash():
 
 
 def push_file(local_path, file):
-    remote_path = '/mnt/sdcard/FuzzDownload'
-    push_cmd = ['adb', 'push', local_path + '/' + file, remote_path + '/' + file]
+    push_cmd = ['adb', 'push', local_path + '/' + file, conf.REMOTE_PATH + file]
     popen_wait(push_cmd)
 
 
@@ -29,39 +28,66 @@ def rm_file(remote_file):
     '''
     清除文件
     '''
-    remote_path = '/mnt/sdcard/FuzzDownload'
-    rm_cmd = ['adb', 'shell', 'rm', remote_path + '/' + remote_file]
+    rm_cmd = ['adb', 'shell', 'rm', conf.REMOTE_PATH + remote_file]
     popen_wait(rm_cmd)
+
+
+def open_file(case):
+    open_cmd = ['adb', 'shell', 'am', 'start', '-W', '-S']
+    open_cmd.extend(['-a', case.target.action])
+    open_cmd.extend(['-c', case.target.category])
+    open_cmd.extend(['-t', case.target.mime_type])
+    open_cmd.extend(['-d', 'file://' + conf.REMOTE_PATH + case.mutant_file])
+    open_cmd.append(case.target.package + "/" + case.target.activity)
+    print'执行:' + to_cmd_str(open_cmd)
+    try:
+        timeout_cmd(open_cmd, timeout=3)
+    except TimeoutError as e:
+        print e
+
+
+def clean_log():
+    log_clean_cmd = ['adb', 'logcat', '-c']
+    print'执行:' + to_cmd_str(log_clean_cmd)
+    popen_wait(log_clean_cmd)
+
+
+def flush_log(case):
+    log_cmd = ['adb', 'logcat', '-d', '-v', 'time', '*:E', '>',
+               conf.LOG_PATH + '/' + 'log-' + case.mutant_file + '.txt']
+
+    print'执行:' + to_cmd_str(log_cmd)
+    popen_wait(log_cmd)
 
 
 def run_job(cases, mutant_files_path, job_id):
     for i, case in enumerate(cases):
-        push_file(mutant_files_path, case.mutant_file)
-
         print_symbol(i.__str__())
-        open_cmd = ['adb', 'shell', 'am', 'start', '-W', '-S']
-        open_cmd.extend(['-a', case.target.action])
-        open_cmd.extend(['-c', case.target.category])
-        open_cmd.extend(['-t', case.target.mime_type])
-        open_cmd.extend(['-d', 'file:///mnt/sdcard/FuzzDownload/' + case.mutant_file])
-        open_cmd.append(case.target.package + "/" + case.target.activity)
+        # push变异文件
+        push_file(mutant_files_path, case.mutant_file)
         # 清空日志
-        log_clean_cmd = ['adb', 'logcat', '-c']
-        print'执行:' + to_cmd_str(log_clean_cmd)
-        popen_wait(log_clean_cmd)
+        clean_log()
         # 执行用例
-        print'执行:' + to_cmd_str(open_cmd)
-        try:
-            timeout_cmd(open_cmd, timeout=3)
-        except TimeoutError as e:
-            print e
+        open_file(case)
         # 记录日志
-        log_cmd = ['adb', 'logcat', '-d', '-v', 'time', '*:E', '>',
-                   conf.LOG_PATH +'/' + 'log-' + case.mutant_file + '.txt']
-        print'执行:' + to_cmd_str(log_cmd)
-        popen_wait(log_cmd)
+        flush_log(case)
+        # 删除变异文件
         rm_file(case.mutant_file)
         print_symbol('')
+
+
+def run_jobs(target):
+    format = 'png'
+    job_path = conf.MUTANTS_PATH + '/' + format + '/'
+    job_num = os.listdir(job_path).__len__()
+    for i in range(1, job_num):
+        mutant_files_path = job_path + i.__str__()
+        mutant_files = os.listdir(mutant_files_path)
+
+        cases = list()
+        for file in mutant_files:
+            cases.append(TestCase(target, file))
+        run_job(cases, mutant_files_path, i)
 
 
 if __name__ == '__main__':
@@ -75,25 +101,14 @@ if __name__ == '__main__':
     #                     '11',
     #                     '22',
     #                     'test/')
-    target1 = TestTarget('com.alensw.PicFolder', 'com.alensw.transfer.TransferActivity',
-                         'android.intent.action.SEND', 'android.intent.category.DEFAULT', 'image/*', 'foo', 'pic',
-                         '11',
-                         '22',
-                         'test/')
-    target2 = TestTarget('com.tencent.mm', 'com.tencent.mm.ui.tools.ShareScreenImgUI',
-                         'android.intent.action.VIEW', 'android.intent.category.DEFAULT', 'image/*', 'foo', 'pic',
-                         '11',
-                         '22',
-                         'test/')
-
-    format = 'png'
-    job_path = conf.MUTANTS_PATH + '/' + format + '/'
-    job_num = os.listdir(job_path).__len__()
-    for i in range(1, job_num):
-        mutant_files_path = job_path + i.__str__()
-        mutant_files = os.listdir(mutant_files_path)
-
-        cases = list()
-        for file in mutant_files:
-            cases.append(TestCase(target2, file))
-        run_job(cases, mutant_files_path, i)
+    # target1 = TestTarget('com.alensw.PicFolder', 'com.alensw.transfer.TransferActivity',
+    #                      'android.intent.action.SEND', 'android.intent.category.DEFAULT', 'image/*', 'foo', 'pic',
+    #                      '11',
+    #                      '22',
+    #                      'test/')
+    target = TestTarget('com.tencent.mm', 'com.tencent.mm.ui.tools.ShareScreenImgUI',
+                        'android.intent.action.VIEW', 'android.intent.category.DEFAULT', 'image/*', 'foo', 'pic',
+                        '11',
+                        '22',
+                        'test/')
+    run_jobs(target)
--- src/utils/conf.py
+++ src/utils/conf.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
 CUSTOM_PATH = '/media/wcx/Ubuntu 14.0/ResearchData'
 DEFAULT_PATH = '..'
+# 父目录
 BASE_PATH = DEFAULT_PATH
+#存放apk的路径
 APK_PATH = BASE_PATH + '/res/apks'
+#存放bugreport的路径
 LOG_PATH = BASE_PATH + '/res/logs'
+#存放种子文件的路径
 SEED_PATH = BASE_PATH + '/res/seeds'
+#存放变异文件的路径
 MUTANTS_PATH = BASE_PATH + '/res/mutants'
+#变异文件推送到远程Android端的路径
+REMOTE_PATH = '/mnt/sdcard/FuzzDownload/'
