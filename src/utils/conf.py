e86121fadf85bbb4e6c30435a9184fbe256b7428
wcx <369808898@qq.com>
2016-06-23 17:50:22 +0800

    修改conf.py中路径,去掉res目录

--- src/utils/conf.py
+++ src/utils/conf.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-CUSTOM_PATH = '/media/wcx/Ubuntu 14.0/ResearchData'
-DEFAULT_PATH = '..'
+CUSTOM_PATH = '/media/wcx/Ubuntu 14.0/ResearchData/res'
+DEFAULT_PATH = '../res'
 # 父目录
 BASE_PATH = DEFAULT_PATH
-#存放apk的路径
-APK_PATH = BASE_PATH + '/res/apks'
-#存放bugreport的路径
-LOG_PATH = BASE_PATH + '/res/logs'
-#存放种子文件的路径
-SEED_PATH = BASE_PATH + '/res/seeds'
-#存放变异文件的路径
-MUTANTS_PATH = BASE_PATH + '/res/mutants'
-#变异文件推送到远程Android端的路径
+# 存放apk的路径
+APK_PATH = BASE_PATH + '/apks'
+# 存放bugreport的路径
+LOG_PATH = BASE_PATH + '/logs'
+# 存放种子文件的路径
+SEED_PATH = BASE_PATH + '/seeds'
+# 存放变异文件的路径
+MUTANTS_PATH = BASE_PATH + '/mutants'
+# 变异文件推送到远程Android端的路径
 REMOTE_PATH = '/mnt/sdcard/FuzzDownload/'
